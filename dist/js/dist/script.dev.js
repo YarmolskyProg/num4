5b054d7f373ae0d438aca359b44d6b3ef67e976f
yarmolsky <vadym.yarmolsky.prog@gmail.com>
2020-09-30 14:30:45 +0300

    co,,it

---
+++ dist/js/dist/script.dev.js
@@ -0,0 +1,117 @@
+"use strict";
+
+$('.dev_elem').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false
+    }
+  }, {
+    breakpoint: 851,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true
+    }
+  }, {
+    breakpoint: 601,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+});
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true
+    }
+  }, {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+});
+$('.mentors').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true
+    }
+  }, {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true
+    }
+  } // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+  ]
+});
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  console.log(123);
+
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
